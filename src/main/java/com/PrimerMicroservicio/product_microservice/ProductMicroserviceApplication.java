346a0d0426e73982baaf6e2772ef3340549b010c
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-11 00:14:15 -0300

    Agregó spring-cloud-starter-netflix-eureka-client y configuró

--- src/main/java/com/PrimerMicroservicio/product_microservice/ProductMicroserviceApplication.java
+++ src/main/java/com/PrimerMicroservicio/product_microservice/ProductMicroserviceApplication.java
@@ -2,8 +2,12 @@ package com.PrimerMicroservicio.product_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableEurekaClient // DA ERROR NO LO RECONOCE
+@EnableDiscoveryClient
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
